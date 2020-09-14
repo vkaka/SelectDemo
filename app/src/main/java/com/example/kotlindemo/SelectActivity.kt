582c6fafde7e4519d33aa824c1d269be8fa48562
zhangce.json <zhangce.json@bytedance.com>
2020-09-14 18:33:48 +0800

    初始化

---
+++ app/src/main/java/com/example/kotlindemo/SelectActivity.kt
@@ -0,0 +1,32 @@
+package com.example.kotlindemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_select.*
+
+/**
+ * Created by zhangce on 2020/9/11
+ */
+class SelectActivity : AppCompatActivity() {
+    private lateinit var selectView: RecyclerView
+    private lateinit var selectViewAdapter: SelectAdapter
+    private lateinit var dataList: ArrayList<DataBean>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select)
+        init()
+    }
+
+    private fun init() {
+        dataList = arrayListOf()
+        for (i in 1..9) {
+            dataList.add(DataBean("条目：" + i))
+        }
+
+        selectView = select_view
+        selectView.layoutManager = GridLayoutManager(this, 3)
+        selectView.adapter = SelectAdapter(this, dataList)
+    }
+}
